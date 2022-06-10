74afa186755fcb6620c65df5ad17327a05b2179f
Ben Anson <24416311+lippirk@users.noreply.github.com>
2022-06-10 11:18:36 +0100

    style: inlinegraphfig

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const onload = () => {
+  // Z button
   const ztoggle = document.getElementById("ztoggle");
   const container = document.getElementsByClassName("container")[0];
   let flag = false;
@@ -8,6 +9,14 @@ const onload = () => {
       : container.classList.add("fillwidth", "pad-lr");
     flag = !flag;
   };
+
+  // inlinegraph figure css
+  const inlinegraphs = document.getElementsByClassName("inlinegraph");
+  for(let el of inlinegraphs) {
+    if (el.tagName === "IMG" && el.parentElement.tagName === "FIGURE") {
+      el.parentElement.classList.add("inlinegraphfig")
+    }
+  }
 };
 
 onload();
